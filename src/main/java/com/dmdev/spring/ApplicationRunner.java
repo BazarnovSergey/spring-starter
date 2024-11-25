fd476702b49e16303d6328357603c78828b4c025
BazarnovSergey <i@sergey-bazarnov.ru>
2024-11-25 22:57:41 +0300

    Lesson 5 XML-based-Configuration

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -5,20 +5,13 @@ import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.ioc.Container;
 import com.dmdev.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        Container container = new Container();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 
-//        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository();
-//        UserService userService = new UserService(userRepository, companyRepository);
-
-//        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//        UserRepository userRepository = container.get(UserRepository.class);
-//        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
-        // TODO: 22.11.2024 userService 
+        System.out.println(context.getBean("p1"));
+        System.out.println(context.getBean("p2"));
     }
 }
