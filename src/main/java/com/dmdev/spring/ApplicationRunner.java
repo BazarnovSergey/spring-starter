a6f2061cabf97bd0e53dba9de828c9d70e97a33a
BazarnovSergey <i@sergey-bazarnov.ru>
2024-11-28 00:05:17 +0300

    Lesson 5 Factory-Method-Injection

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -9,9 +9,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-
-        var connectionPool = context.getBean("p1");
+        var context = new ClassPathXmlApplicationContext("application.xml");
+//      clazz -> String -> Map<String, Object>
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
         System.out.println(connectionPool);
+
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
