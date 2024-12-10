63e6dd906223059f6043606c4c31726783ad4f80
BazarnovSergey <i@sergey-bazarnov.ru>
2024-12-10 23:43:58 +0300

    Lesson 18 Autowired-_-_Value

--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -5,29 +5,43 @@ import com.dmdev.spring.bpp.InjectBean;
 import com.dmdev.spring.bpp.Transaction;
 import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
-public class CompanyRepository implements CrudRepository<Integer, Company>{
-    @InjectBean
-    private ConnectionPool connectionPool;
+public class CompanyRepository implements CrudRepository<Integer, Company> {
+
+    //    @Resource(name = "pool1")
+//    @Qualifier("pool1")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
-    public void init() {
-        System.out.println("CompanyRepository init");
+    private void init() {
+        System.out.println("init company repository");
     }
 
     @Override
     public Optional<Company> findById(Integer id) {
-        System.out.println("findById method called");
+        System.out.println("findById method...");
         return Optional.of(new Company(id));
     }
 
     @Override
     public void delete(Company entity) {
-        System.out.println("delete method called");
+        System.out.println("delete method...");
+    }
+
+    @Autowired
+    public void setPool1(ConnectionPool pool1) {
+        this.pool1 = pool1;
     }
 }
