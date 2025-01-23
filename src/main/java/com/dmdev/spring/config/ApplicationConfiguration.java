41f3568ef9ed6c5fabd9ddad168f6181aff6650c
user <i@sergey-bazarnov.ru>
2025-01-23 23:32:12 +0300

    Lesson 26 -_Bean.-Часть-1

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -17,4 +21,15 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
         })
 public class ApplicationConfiguration {
+
+        @Bean("pool2")
+        @Scope(BeanDefinition.SCOPE_SINGLETON)
+        public ConnectionPool pool2(@Value("${db.username}") String username) {
+                return new ConnectionPool(username, 20);
+        }
+
+        @Bean
+        public UserRepository userRepository2(ConnectionPool pool2) {
+                return new UserRepository(pool2);
+        }
 }
