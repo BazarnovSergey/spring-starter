2e00cae3df2b10e5ecb1a5c9b608f49b28e9a82e
user <i@sergey-bazarnov.ru>
2025-01-29 00:44:37 +0300

    Lesson 30 vent-Listeners.-Часть-2

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -6,6 +6,7 @@ import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.database.repository.CrudRepository;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.ioc.Container;
+import com.dmdev.spring.service.CompanyService;
 import com.dmdev.spring.service.UserService;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -30,8 +31,8 @@ public class ApplicationRunner {
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
 
-            var companyRepository = context.getBean("companyRepository", CrudRepository.class);
-            System.out.println(companyRepository.findById(1));
+            var companyService = context.getBean("companyService", CompanyService.class);
+            System.out.println(companyService.findById(1));
         }
     }
 }
--- src/main/java/com/dmdev/spring/service/CompanyService.java
+++ src/main/java/com/dmdev/spring/service/CompanyService.java
@@ -1,13 +1,34 @@
 package com.dmdev.spring.service;
 
+import com.dmdev.spring.database.entity.Company;
+import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.dto.CompanyReadDto;
+import com.dmdev.spring.listener.entity.AccessType;
+import com.dmdev.spring.listener.entity.EntityEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
 
+    private final CrudRepository<Integer, Company> companyRepository;
     private final UserService userService;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public CompanyService(UserService userService) {
+    public CompanyService(CrudRepository<Integer, Company> companyRepository, UserService userService,
+                          ApplicationEventPublisher applicationEventPublisher) {
+        this.companyRepository = companyRepository;
         this.userService = userService;
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    public Optional<CompanyReadDto> findById(Integer id) {
+        return companyRepository.findById(id)
+                .map(entity -> {
+                    applicationEventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
+                    return new CompanyReadDto(entity.id());
+                });
     }
 }
