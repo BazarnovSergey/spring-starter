fdfe592de8502aa64016ccf7185491b0345c1549
user <i@sergey-bazarnov.ru>
2025-02-16 23:35:41 +0300

    Lesson 59 Special-parameters

--- src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package com.dmdev.spring.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -18,5 +24,17 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query( value = "SELECT u.* FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u " +
+            "set u.role = :role " +
+            "where u.id in(:ids)")
+    int updateRole(@Param("role") Role role, @Param("ids") List<Long> ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
 
 }
--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,9 +1,19 @@
 package com.dmdev.spring.integration.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
+import com.dmdev.spring.database.entity.User;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @RequiredArgsConstructor
@@ -12,9 +22,47 @@ class UserRepositoryTest {
     private final UserRepository userRepository;
 
     @Test
+    void checkPageable() {
+        var pageable = PageRequest.of(1, 2, Sort.by("id"));
+        var result = userRepository.findAllBy(pageable);
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void checkSort() {
+        var sortBy = Sort.sort(User.class);
+        var sort = sortBy.by(User::getFirstname)
+                .and(sortBy.by(User::getLastname));
+        var sortById = Sort.by("firstName").and(Sort.by("lastName"));
+        var allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
     void checkQueries() {
         var users = userRepository.findAllBy("a", "ov");
         System.out.println(users);
+    }
+
+    @Test
+    void checkUpdate() {
+        var ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+
+        List<Long> ids = List.of(1L, 2L);
+        int resultCount = userRepository.updateRole(Role.USER, ids);
+        assertEquals(2, resultCount);
+
+        var theSameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+
+    }
+
+    @Test
+    void checkFirstTop() {
+        var topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
     }
 
 }
