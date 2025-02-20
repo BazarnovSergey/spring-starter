ce04e2d735f37fd86caedc44b28e2645a4d84000
user <i@sergey-bazarnov.ru>
2025-02-21 00:33:33 +0300

    Lesson 63 Projection

--- src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -2,16 +2,16 @@ package com.dmdev.spring.database.repository;
 
 import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
+import com.dmdev.spring.dto.PersonalInfo2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +26,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "where u.firstname like %:firstname% and u.lastname like %:lastname%")
     List<User> findAllBy(@Param("firstname") String firstname, @Param("lastname") String lastname);
 
-    @Query( value = "SELECT u.* FROM users u WHERE u.username = :username",
+    @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
 
@@ -38,11 +38,24 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findTopByOrderByIdDesc();
 
+    @QueryHints(@QueryHint(name = "org.hibernate.fetchSize", value = "50"))
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
 
     @EntityGraph(attributePaths = {"company"})
     @Query(value = "select u from User u",
-    countQuery = "select count (distinct u.firstname) from User u")
+            countQuery = "select count (distinct u.firstname) from User u")
     Page<User> findAllBy(Pageable pageable);
 
+//    List<PersonalInfo> findAllByCompanyId(Integer companyId);
+//    <T> List<T> findAllByCompanyId(Integer companyId, Class<T> clazz);
+
+
+    @Query(value = "SELECT firstname," +
+            "lastname," +
+            "birth_date birthDate "  +
+            "FROM users " +
+            "WHERE company_id = :companyId",
+            nativeQuery = true)
+    List<PersonalInfo2> findAllByCompanyId(@Param("companyId") Integer companyId);
 }
