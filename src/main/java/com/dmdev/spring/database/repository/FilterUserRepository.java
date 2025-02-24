d348d46e3ecf8fec7ab24fa1e117a54b98d2399d
user <i@sergey-bazarnov.ru>
2025-02-24 23:16:15 +0300

    Lesson 69 Batch-size

--- src/main/java/com/dmdev/spring/database/repository/FilterUserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/FilterUserRepository.java
@@ -13,4 +13,8 @@ public interface FilterUserRepository {
     List<User> findAllByFilter(UserFilter filter);
 
     List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
+
+    void updateCompanyAndRole(List<User> users);
+
+    void updateCompanyAndRoleNamed(List<User> users);
 }
