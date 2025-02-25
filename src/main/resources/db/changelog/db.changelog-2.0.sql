05b58c2a17cea7575de43475655f61de174541ff
user <i@sergey-bazarnov.ru>
2025-02-25 22:45:43 +0300

    Lesson 73 liquibase

---
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+
+--changeset sbazarnov:1
+ALTER TABLE users
+    ADD COLUMN created_at TIMESTAMP;
+
+ALTER TABLE users
+    ADD COLUMN modified_at TIMESTAMP;
+
+ALTER TABLE users
+    ADD COLUMN created_by VARCHAR(32);
+
+ALTER TABLE users
+    ADD COLUMN last_modified_by VARCHAR(32);
