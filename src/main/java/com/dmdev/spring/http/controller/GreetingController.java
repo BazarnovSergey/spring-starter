6f45e4a3f865a9881eb840289fe03e204b219479
user <i@sergey-bazarnov.ru>
2025-03-06 22:58:24 +0300

    Lesson 79 Model

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,22 +1,47 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.dto.UserReadDto;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
     @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    public ModelAndView hello(ModelAndView modelAndView,
+                              HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+
+        return modelAndView;
+    }
+
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello2(ModelAndView modelAndView,
+                               HttpServletRequest request,
+                               @Param("age") Integer age,
+                               @RequestHeader("accept") String accept,
+                               @CookieValue("JSESSIONID") String jsessionId,
+                               @PathVariable("id") Integer id) {
+        String ageParamValue = request.getParameter("age");
+        String acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
+
+
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user,
+                            ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
