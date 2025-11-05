3705e8d3c9254d245304f9bbbbab73ca7891cbc1
sergey_101 <i@sergey-bazarnov.ru>
2025-11-05 22:30:13 +0300

    Lesson 105 Logout

--- src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
+++ src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.dmdev.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -17,6 +16,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
 //                .httpBasic(Customizer.withDefaults());
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login
                         .loginPage("/login")
                         .defaultSuccessUrl("/users")
